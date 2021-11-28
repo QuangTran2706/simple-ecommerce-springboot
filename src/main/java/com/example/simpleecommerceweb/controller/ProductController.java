ddc273b4cbd54393865aceab8306d31c5f0a5a04
QuangTran2706 <48042533+QuangTran2706@users.noreply.github.com>
2021-11-28 16:14:05 +0700

    update CORS

--- src/main/java/com/example/simpleecommerceweb/controller/ProductController.java
+++ src/main/java/com/example/simpleecommerceweb/controller/ProductController.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(maxAge = 3600)
 @RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
